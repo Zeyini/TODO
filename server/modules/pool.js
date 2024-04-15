6c4764377bace5ab8ec0ea7d8281c969a8cd8bb9
Zeyini Mohammad <Zeyinim@outlook.com>
2024-04-15 09:22:29 -0500

    changed the todo name to databaseName which is assign the value of my databasename

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
-    database:'weekend-to-do-app',
+    database:databaseName,
     allowExitOnIdle: true 
 })
 
